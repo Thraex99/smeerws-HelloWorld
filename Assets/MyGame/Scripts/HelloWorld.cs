efecc152d42d0bff42b325c5120d2f8bbe554775
Thraex99 <mgaro99@gmail.com>
2019-03-06 17:30:24 +0100

    update script clean

--- Assets/MyGame/Scripts/HelloWorld.cs
+++ Assets/MyGame/Scripts/HelloWorld.cs
@@ -5,12 +5,14 @@ using System;
 using UnityEngine;
 
 
-public class HelloWorld : MonoBehaviour {
+public class HelloWorld : MonoBehaviour 
+{
 
 
 	// Use this for initialization
-	void Start () {
-        string hw = "Hello World (hw)";
+	void Start () 
+	{
+        string helloWorldText = "Hello World (helloWorldText)";
 
         print("(print) Hello World");
         Debug.Log("(Debug.Log) Hello World");
@@ -19,13 +21,14 @@ public class HelloWorld : MonoBehaviour {
                                                               //through Unity Engine: Debug.Log, ...
                                                               //Unity doesn't display the standard C# Console.
 
-        print("(print - hw) " + hw);
-        Debug.Log("(Debug.Log - hw ) " + hw);
-        Console.WriteLine("Console.WriteLine - hw" + hw);
+        print("(print - helloWorldText) " + helloWorldText);
+        Debug.Log("(Debug.Log - helloWorldText ) " + helloWorldText);
+        Console.WriteLine("Console.WriteLine - helloWorldText" + helloWorldText);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
 	}
 }
